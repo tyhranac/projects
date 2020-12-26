733167efd474a23b196254bd47a2a1ade1fcf988
ty.hranac <ty.hranac@gmail.com>
2020-12-26 15:20:47 -0800

    myTree

---
+++ myTree.py
@@ -0,0 +1,25 @@
+#!/path/to/python/interpreter
+import os
+from colorama import init
+
+init()
+
+root = os.getcwd()
+
+def tree(path):
+	subdirs = [entry for entry in os.scandir(path) if entry.is_dir()]
+	if not subdirs:
+		depth = path.count('\\') - root.count('\\')
+		for entry in os.scandir(path):
+			print('\033[1;37m{}|__{}'.format('  '*depth, entry.name))
+	else:
+		for entry in subdirs:
+			depth = path.count('\\') - root.count('\\')
+			print('\033[1;33m{}{}'.format('  '*depth, entry.name))
+			tree(entry.path)
+
+tree(root)
+
+for entry in os.scandir(root):
+	if not entry.is_dir():
+		print(entry.name)
